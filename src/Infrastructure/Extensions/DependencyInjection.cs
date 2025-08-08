b3443950c858bbf56a1dbbdd3cb54740673d4325
igao94 <igao998@gmail.com>
2025-08-08 13:37:03 +0200

    Add SQL Server database and in-memory database for testing

---
+++ src/Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddDbContext<AppDbContext>(opt =>
+        {
+            bool useInMemoryDatabase = config.GetValue<bool>("UseInMemoryDatabase");
+
+            if (useInMemoryDatabase)
+            {
+                opt.UseInMemoryDatabase("InMemoryDatabase");
+            }
+            else
+            {
+                opt.UseSqlServer(config.GetConnectionString("SqlServer"));
+            }
+        });
+
+        return services;
+    }
+}
