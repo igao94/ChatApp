560e8c88ba0a3b676cc26935a3a8c33d03ab0887
igao94 <igao998@gmail.com>
2025-08-09 13:03:37 +0200

    Add and configure AppUser entity

---
+++ src/Infrastructure/Config/AppUserEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Config;
+
+internal sealed class AppUserEntityConfiguration : IEntityTypeConfiguration<AppUser>
+{
+    public void Configure(EntityTypeBuilder<AppUser> builder)
+    {
+        builder.Property(u => u.Name).IsRequired();
+
+        builder.Property(u => u.Email).IsRequired();
+
+        builder.Property(u => u.PasswordHash).IsRequired();
+
+        builder.Property(u => u.PasswordSalt).IsRequired();
+    }
+}
--- src/Infrastructure/Database/AppDbContext.cs
+++ src/Infrastructure/Database/AppDbContext.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Entites;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Database;
 
 internal sealed class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+    internal DbSet<AppUser> Users { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
 }
