e4b3a8293caec66b95896e0382b9d5c918bff40c
igao94 <igao998@gmail.com>
2025-08-12 12:01:30 +0200

    Add generic Result<T> class to represent handler responses

--- src/Shared/Result.cs
+++ src/Shared/Result.cs
@@ -2,17 +2,16 @@
 
 public sealed class Result<T>
 {
+    private Result()
+    {
+
+    }
+
     public bool IsSuccess { get; private set; }
     public T? Value { get; private set; }
     public string? Error { get; private set; }
-    public int StatusCode { get; private set; }
 
     public static Result<T> Success(T value) => new() { IsSuccess = true, Value = value };
 
-    public static Result<T> Failure(string error, int statusCode) => new()
-    {
-        IsSuccess = false,
-        Error = error,
-        StatusCode = statusCode
-    };
+    public static Result<T> Failure(string error) => new() { IsSuccess = false, Error = error };
 }
