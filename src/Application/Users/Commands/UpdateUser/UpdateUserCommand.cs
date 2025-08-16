706cf0a435c2ee4ca47f542b6bc89ca8dbe3e7c4
igao94 <igao998@gmail.com>
2025-08-16 12:26:30 +0200

    Refactor UpdateUserHandler to use UserContext for current user

--- src/Application/Users/Commands/UpdateUser/UpdateUserCommand.cs
+++ src/Application/Users/Commands/UpdateUser/UpdateUserCommand.cs
@@ -3,5 +3,5 @@ using Shared;
 
 namespace Application.Users.Commands.UpdateUser;
 
-public sealed record UpdateUserCommand(Guid Id, string? About) : IRequest<Result<Unit>>;
+public sealed record UpdateUserCommand(string? About) : IRequest<Result<Unit>>;
 
