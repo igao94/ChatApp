dd0f1e2ed2dd4d4bb533e604685c506e63031f5f
igao94 <igao998@gmail.com>
2025-09-03 10:49:18 +0200

    Implement deleting messages functionality

---
+++ src/Application/Messages/Commands/DeleteMessage/DeleteMessageCommand.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Shared;
+
+namespace Application.Messages.Commands.DeleteMessage;
+
+public sealed record DeleteMessageCommand(Guid Id) : IRequest<Result<Unit>>;
+
---
+++ src/Application/Messages/Commands/DeleteMessage/DeleteMessageHandler.cs
@@ -0,0 +1,48 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Repositories;
+using MediatR;
+using Shared;
+
+namespace Application.Messages.Commands.DeleteMessage;
+
+internal sealed class DeleteMessageHandler(IUnitOfWork unitOfWork,
+    IUserContext userContext) : IRequestHandler<DeleteMessageCommand, Result<Unit>>
+{
+    public async Task<Result<Unit>> Handle(DeleteMessageCommand request, CancellationToken cancellationToken)
+    {
+        var currentUserId = userContext.UserId;
+
+        var message = await unitOfWork.MessageRepository.GetByIdAsync(request.Id);
+
+        if (message is null)
+        {
+            return Result<Unit>.Failure("Message not found.");
+        }
+
+        if (message.SenderId != currentUserId && message.RecipientId != currentUserId)
+        {
+            return Result<Unit>.Failure("You can't delete this message.");
+        }
+
+        if (message.SenderId == currentUserId)
+        {
+            message.SenderDeleted = true;
+        }
+
+        if (message.RecipientId == currentUserId)
+        {
+            message.RecipientDeleted = true;
+        }
+
+        if (message is { SenderDeleted: true, RecipientDeleted: true })
+        {
+            unitOfWork.MessageRepository.Delete(message);
+        }
+
+        var result = await unitOfWork.SaveChangesAsync();
+
+        return result
+            ? Result<Unit>.Success(Unit.Value)
+            : Result<Unit>.Failure("Failed to delete message.");
+    }
+}
