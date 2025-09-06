5b5ab4f7f1e370475791e32adba332c9e69be356
igao94 <igao998@gmail.com>
2025-09-06 12:36:01 +0200

    Add generic cursor-based pagination support

---
+++ src/Shared/Pagination/CursorPagination.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.Pagination;
+
+public sealed class CursorPagination<T, TCursor>
+{
+    public IReadOnlyList<T> Items { get; set; } = [];
+    public TCursor? Cursor { get; set; }
+}
