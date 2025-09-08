6cd8b77157e520b141a53e1784ad4407e4c66779
igao94 <igao998@gmail.com>
2025-09-08 13:19:13 +0200

    Replace hard delete with user deactivation (soft delete)

---
+++ src/Application/Users/Commands/DeactivateUser/DeactivateUserCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Shared;
+
+namespace Application.Users.Commands.DeactivateUser;
+
+public sealed record DeactivateUserCommand : IRequest<Result<Unit>>;
---
+++ src/Application/Users/Commands/DeactivateUser/DeactivateUserHandler.cs
@@ -0,0 +1,29 @@
+﻿using Application.Abstractions.Authentication;
+using Application.Abstractions.Repositories;
+using Application.Helpers;
+using MediatR;
+using Shared;
+
+namespace Application.Users.Commands.DeactivateUser;
+
+internal sealed class DeactivateUserHandler(IUnitOfWork unitOfWork,
+    IUserContext userContext) : IRequestHandler<DeactivateUserCommand, Result<Unit>>
+{
+    public async Task<Result<Unit>> Handle(DeactivateUserCommand request, CancellationToken cancellationToken)
+    {
+        var userResult = await unitOfWork.GetUserByIdAsync(userContext.UserId);
+
+        if (userResult.IsFailure)
+        {
+            return Result<Unit>.Failure(userResult.Error!);
+        }
+
+        var user = userResult.Value!;
+
+        user.IsActive = false;
+
+        return await unitOfWork.SaveChangesAsync()
+            ? Result<Unit>.Success(Unit.Value)
+            : Result<Unit>.Failure("Failed to deactiavate user.");
+    }
+}
